cc7d511f3b2137a311009c78c66c46af16dfd291
jtrtj <jt.r@live.com>
2018-06-30 11:15:26 -0600

    update controller to account for age query params

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,11 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
-    @comedians = Comedian.all.includes(:specials)
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age]).includes(:specials)
+    else
+      @comedians = Comedian.all.includes(:specials)
+    end
     @average_age = @comedians.avg_age
     erb :'comedians/index'
   end
